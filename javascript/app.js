b6d2eb1c3a413c5810342136f9a482a444ae98a8
kyawswarhtun6 <kyawswarhtun213@gmail.com>
2023-08-20 13:59:14 +0630

    19-8-2023

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,13 @@
 // start jquery area
 
-   $(document).ready(function(){
+$(window).on("load",function(){
+    // console.log(true);
+    $("#preloader").delay(500).fadeOut("slow");
+})
+
+
+
+$(document).ready(function(){
     $("#banner_coursel").owlCarousel({
         items: 1,
         loop: true,
@@ -20,7 +27,68 @@
     
        })
     
-   })
+    $(".search_btn").click(function(){
+        $("#search_modal").addClass("active");
+    })   
+
+    $(window).click(function(e){
+        console.log(e.target);
+        if(e.target.classList.contains("search_modal")){
+            $("#search_modal").removeClass("active");
+        }
+        
+    })
+
+    $(window).scroll(function(e){
+        // console.log($(this).scrollTop());
+        var getscrollamount = $(this).scrollTop();
+        console.log(getscrollamount);
+        if(getscrollamount>100){
+            $(".navbar").addClass("active");
+        }else{
+            $(".navbar").removeClass("active");
+
+        }
+
+        if(getscrollamount>900){
+            $(".arrival_content_container").addClass("active");
+        }else{
+            $(".arrival_content_container").removeClass("active");
+
+        }
+
+        if(getscrollamount>1850){
+            $(".about_title_container").addClass("active");
+        }else{
+            $(".about_title_container").removeClass("active");
+
+        }
+
+        if(getscrollamount>1950){
+            $(".about_title_container").addClass("active");
+        }else{
+            $(".about_title_container").removeClass("active");
+
+        }
+
+
+        if(getscrollamount>2600){
+            $(".established_content_container").addClass("active");
+        }else{
+            $(".established_content_container").removeClass("active");
+
+        }
+
+        if(getscrollamount>3300){
+            $(".our_story_section").addClass("active");
+        }else{
+            $(".our_story_section").removeClass("active");
+
+        }
+        
+    })
+
+})
 
   
 
